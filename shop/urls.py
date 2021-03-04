fae8f099230a23c731dcd0c4423a5db7a31cf7e8
tandalalam <maziar56bnd@gmail.com>
2021-03-04 13:38:55 +0330

    :((((()))))

--- shop/urls.py
+++ shop/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from shop.views import main_page
+from shop.views import main_page, register_page
 
 urlpatterns = [
-    path('', main_page, name='main_page')
+    path('', main_page, name='main_page'),
+    path('register/', register_page, name='register_page' )
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,27 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
+
 
 # Create your views here.
 
 
 def main_page(request):
     return render(request, 'shop/main_page.html')
+
+
+def register_page(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+        email = request.POST.get('email')
+        user = User.objects.create_user(username=username,
+                                        email=email,
+                                        password=password1,
+                                        first_name=first_name,
+                                        last_name=last_name
+                                        )
+        user.save()
+    return render(request, 'shop/register.html')
